79460da4f51cab0100c0d981a41c632b5a1ccfcf
Unoriginalmiguel <jimenez_miguel_alexi@student.smc.edu>
2021-05-11 14:17:39 -0700

    Catching up on HW

--- week-11/in-class/canvasStatic.js
+++ week-11/in-class/canvasStatic.js
@@ -1,8 +1,9 @@
-const canvas = document.getElementById(staticCanvas);
-const cntx = canvas.getContext('2d');
+const canvas1 = document.getElementById("staticCanvas")
+const cntx = canvas1.getContext('2d');
 
 cntx.fillStyle = "darkblue";
 cntx.fillRect(20, 140, 200, 300);
+
 
 cntx.fillStyle = "orange";
 
@@ -10,17 +11,17 @@ cntx.beginPath();
 cntx.moveTo(200, 75); //x, then y
 cntx.lineTo(350, 200);
 cntx.lineTo(150, 100);
-cntx.lineTo(200,75);
+cntx.lineTo(200, 75);
 cntx.fill();
 cntx.closePath();
 
 cntx.beginPath();
 cntx.fillStyle = "teal";
-cntx.arc(500, 300, 100, 0, Math.PI*2, true);
+cntx.arc(500, 300, 100, 0, Math.PI*2/3, true);
 cntx.fill();
 cntx.closePath();
 
-cntx.fillStyle = "red";
-cntx.font = "48px Arial, Helvetica, sans-serif";
+cntx.strokeStyle = "red";
+cntx.font = "49px Arial, Helvetica, sans-serif";
 cntx.textAlign = "center";
 cntx.strokeText("Text on Canvas!", 400, 400);
