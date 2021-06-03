61cf442c6146225924a7fa5c8b1c274295f733cd
Unoriginalmiguel <jimenez_miguel_alexi@student.smc.edu>
2021-06-03 16:03:25 -0700

    updated final project

---
+++ week-14-final/js/script.js
@@ -0,0 +1,21 @@
+    var count = 0;
+        var btn = document.querySelector("#BTNclick");
+        var disp = document.getElementById("display");
+  
+        btn.onclick = function () {
+            count++;
+            disp.innerHTML = count;
+        }
+
+
+    // var count = 0;
+    //     var btn = document.getElementById("btn");
+    //     var disp = document.getElementById("display");
+  
+    //     btn.onclick = function () {
+    //         count++;
+    //         disp.innerHTML = count;
+    //     }
+
+
+
